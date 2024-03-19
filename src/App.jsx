3b638c1d14535110f023c6a296754bcbdfcc6a9a
Lucky Kumar <lucky.kumar@lpu.in>
2024-03-19 19:47:30 +0530

    animations added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/common/Header/Header";
 import Footer from "./components/common/Footer/Footer";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return (
     <div className="min-h-screen flex flex-wrap content-between">
       <div className="w-full block">
